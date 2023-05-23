7f236ed7a61bb996ae57ed4cf33d5dd296ed52b3
Úku Parts <uku.parts@gmail.com>
2023-05-23 17:16:18 +0300

    improve popups

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var startButton = document.getElementById("startButton");
-    var closeButton = document.getElementById("closeButton");
     var overlay = document.getElementById("overlay");
     var popup = document.getElementById("popup");
     
     startButton.addEventListener("click", function() {
         overlay.style.display = "block";
         popup.style.display = "block";
-    });
-    
-    closeButton.addEventListener("click", function() {
-        overlay.style.display = "none";
-        popup.style.display = "none";
     });
     
     overlay.addEventListener("click", function() {
